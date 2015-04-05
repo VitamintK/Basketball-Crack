4aa276c54d0892a2a150e84b89008e3671a915f2
KEvin Wang <kevinwang@kevinwang.us>
2015-04-05 08:53:25 -0700

    :spaghetti:

---
+++ app/bball.py
@@ -0,0 +1,76 @@
+from flask import Flask
+from flask import render_template, request, jsonify
+import json
+import random
+import zlib
+import os
+
+
+app = Flask(__name__)
+
+HEADERS = ["Season", "Age", "Tm", "Lg", "Pos", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS"]
+
+players = os.listdir("json/")
+
+def generate_hashes():
+    hashdict = dict()
+    for i in players:
+        hashdict[str(crc(i[:-5]))] = i[:-5]
+    with open('hdict', 'w') as hdict:
+        json.dump(hashdict, hdict)
+    return hashdict
+
+def pick_a_year():
+    player = random.choice(players)
+    player_name = player[:-5]
+    with open('json/{}'.format(player)) as pjson:
+        player_json = json.load(pjson)
+    rows = []
+    for row in player_json['per_game'][1:]:
+        if row[0] == 'Career':
+            break
+        else:
+            rows.append(row)
+    return [random.choice(rows)], player_name
+
+def crc(name):
+    return zlib.crc32(bytes(name.lower(), 'UTF-8'))
+
+hashdict = generate_hashes()
+
+@app.route('/')
+def hello_world():
+    table, player_name = pick_a_year()
+    pnum = crc(player_name)
+    return render_template("index.html", headers = HEADERS, table=table, pnum=pnum)
+
+@app.route('/submit', methods=['GET'])
+def submit():
+    player = request.args.get('player_name');
+    pnum = request.args.get('p_num');
+    print(player)
+    print(crc(player))
+    print(pnum)
+    if crc(player) == int(pnum):
+        table, player_name = pick_a_year()
+        return jsonify(successCode = '1', pnum = crc(player_name), stats = render_template("table.html", headers = HEADERS, table=table))
+    else:
+        return jsonify(successCode = '0')
+
+@app.route('/giveup', methods=['GET'])
+def giveup():
+    pnum = request.args.get('p_num');
+    old_player_name = hashdict[pnum]
+    table, player_name = pick_a_year()
+    return jsonify(pnum = crc(player_name), player_name = old_player_name, stats = render_template("table.html", headers = HEADERS, table=table))
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
+if __name__ == '__main__':
+    app.run(debug = True)#host = '0.0.0.0')
+
+
+#print(json.dumps(player_json, sort_keys=True, indent=4, separators=(',', ': ')))
---
+++ app/static/js.js
@@ -0,0 +1,67 @@
+var print_result = function(wl){
+	var message;
+	if(wl == 1){
+		message = '<h1 class="alert alert-success">YOU ARE RIGHT.</h1>';
+	}else{
+		message = '<h1 class="alert alert-danger">YOU ARE WRONG!!!!</h1>';
+	}
+	$('#resMessage').html(message);
+}
+
+var display_loss = function(player_name, stats){
+	message = '<h1 class="alert alert-danger">YOU GAVE UP.  The player was ' + player_name + '!!!!</h1>';
+	$('#resMessage').html(message);
+	nextbtn = '<button id="next" class="btn btn-default"> NEXT </button>'
+	currentcontents=$('#input_stuff').html();
+	$('#input_stuff').html(nextbtn);
+	$('#next').click(function(){
+		$('#input_stuff').html(currentcontents);
+		replace_table(stats);
+		prep_buttons();
+	})
+}
+
+var replace_table = function(newtable){
+	$('#stattable').html(newtable);
+}
+
+var prep_buttons = function(){
+	$('#go_btn').click(function(){
+		var player = $('#player').val();
+		$.ajax({
+			url: "/submit",
+			data: {
+				player_name: player,
+				p_num: pnum
+			},
+			success: function(data){
+				print_result(data.successCode);
+				if(data.successCode == 1){
+					replace_table(data.stats);
+					pnum = data.pnum;
+				}
+				console.log(data);
+			},
+			error: function(data){
+				console.log(data);
+			}
+		});
+	});
+
+	$('#give_btn').click(function(){
+		$.ajax({
+			url: "/giveup",
+			data: {
+				p_num: pnum
+			},
+			success: function(data){
+				pnum = data.pnum;
+				display_loss(data.player_name, data.stats);
+			}
+		});
+	});
+}
+
+$( document ).ready(function(){
+	prep_buttons();
+});
