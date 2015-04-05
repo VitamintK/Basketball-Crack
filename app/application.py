45626f1e0133afe411aff54106cbb6e81db0d5e5
KEvin Wang <kevinwang@kevinwang.us>
2015-04-05 10:53:56 -0700

    new

--- app/application.py
+++ app/application.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 HEADERS = ["Season", "Age", "Tm", "Lg", "Pos", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS"]
 
-players = os.listdir("json/")
+players = os.listdir("super_json/")
 
 def generate_hashes():
     hashdict = dict()
@@ -23,7 +23,7 @@ def generate_hashes():
 def pick_a_year():
     player = random.choice(players)
     player_name = player[:-5]
-    with open('json/{}'.format(player)) as pjson:
+    with open('super_json/{}'.format(player)) as pjson:
         player_json = json.load(pjson)
     rows = []
     for row in player_json['per_game'][1:]:
@@ -38,13 +38,13 @@ def crc(name):
 
 hashdict = generate_hashes()
 
-@application.route('/')
+@app.route('/')
 def hello_world():
     table, player_name = pick_a_year()
     pnum = crc(player_name)
     return render_template("index.html", headers = HEADERS, table=table, pnum=pnum)
 
-@application.route('/submit', methods=['GET'])
+@app.route('/submit', methods=['GET'])
 def submit():
     player = request.args.get('player_name');
     pnum = request.args.get('p_num');
@@ -57,20 +57,20 @@ def submit():
     else:
         return jsonify(successCode = '0')
 
-@application.route('/giveup', methods=['GET'])
+@app.route('/giveup', methods=['GET'])
 def giveup():
     pnum = request.args.get('p_num');
     old_player_name = hashdict[pnum]
     table, player_name = pick_a_year()
     return jsonify(pnum = crc(player_name), player_name = old_player_name, stats = render_template("table.html", headers = HEADERS, table=table))
 
-@application.route('/shutdown', methods=['POST'])
+@app.route('/shutdown', methods=['POST'])
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
 
 if __name__ == '__main__':
-    application.run()#host = '0.0.0.0')
+    app.run()#host = '0.0.0.0')
 
 
 #print(json.dumps(player_json, sort_keys=True, indent=4, separators=(',', ': ')))
